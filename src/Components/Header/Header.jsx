a46b38910059a65822490c12c06763cbc60daef4
Yehor Kyslynskyi <kislinskiycr7@gmail.com>
2021-10-26 15:03:38 +0300

    add all files

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './header.module.scss'
+
+const Header = () => (
+  <div className={styles.header}>
+    <div className={styles.container}>
+      <div className={styles.title}>To Do List </div>
+    </div>
+  </div>
+)
+
+export default Header
