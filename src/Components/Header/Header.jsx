f70086f8a5ae8c8dcfc163e6f40568f2820b5859
Yehor Kyslynskyi <kislinskiycr7@gmail.com>
2021-11-08 12:31:38 +0200

    add changes for pull request

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useSelector } from 'react-redux'
 import styles from './header.module.scss'
 import TaskCounter from './TaskCounter/TaskCounter'
 
-const Header = () => (
-  <div className={styles.header}>
-    <div className={styles.container}>
-      <div className={styles.menuAndButtons}>
-        <div className={styles.icon}>
-          <span />
+const Header = () => {
+  const tasks = useSelector((state) => state.tasks.tasks)
+  const a = 0
+  const businessTasksCount = useMemo(
+    () => tasks.filter((task) => task.type === 'Business').length || 0,
+    [tasks],
+  )
+
+  const personalTasksCount = useMemo(
+    () => tasks.filter((task) => task.type === 'Personal').length || 0,
+    [tasks],
+  )
+
+  const date = new Date()
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.menuAndButtons}>
+          <div className={styles.icon}>
+            <span />
+          </div>
         </div>
-      </div>
-      <div className={styles.titleAndTaskCounters}>
-        <div className={styles.title}>Your Things</div>
-        <div className={styles.tasksContainer}>
-          <TaskCounter count={26} description="Personal" />
-          <TaskCounter count={10} description="Business" />
+        <div className={styles.titleAndTaskCounters}>
+          <div className={styles.title}>Your Things</div>
+          <div className={styles.tasksContainer}>
+            <TaskCounter count={personalTasksCount} description="Personal" />
+            <TaskCounter count={businessTasksCount} description="Business" />
+          </div>
         </div>
-      </div>
-      <div className={styles.dateAndProgress}>
-        <div className={styles.date}>Oct 26, 2021</div>
+        <div className={styles.dateAndProgress}>
+          <div className={styles.date}>{date.toDateString()}</div>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
